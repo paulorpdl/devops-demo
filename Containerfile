35f97a08b275d4cab7ee4981ed423d681a6e6e5f
Paulo Pinto <paulorpinto@gmail.com>
2025-06-17 15:32:25 -0500

    improvements in contenerization.

--- Containerfile
+++ Containerfile
@@ -2,9 +2,6 @@ FROM python:3.11.3-slim
 
 LABEL org.opencontainers.image.description "DevOps skills demonstration with Python, Containers & Kubernetes"
 
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
-
 RUN pip install --upgrade pip \
     && useradd -m app
 
@@ -14,6 +11,10 @@ COPY --chown=app:app . /app
 # Switch to app user and home directory
 USER 1000
 WORKDIR /app
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV PATH="$PATH:/home/app/.local/bin"
 
 # Build dependencies
 RUN pip install -r requirements.txt \
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,52 @@
 
 set -e
 
-# Detecting available cores
-CPU_CORES=$(nproc)
-
+# Default values
 : "${APP_MODULE:=demo.asgi}"
-: "${WORKERS:=$(( 2 * CPU_CORES + 1))}"
-: "${THREADS:=2}"
 : "${BIND:=0.0.0.0:8000}"
+: "${GUNICORN_WORKERS_PER_CORE:=2}"
+: "${GUNICORN_MAX_WORKERS:=16}"  # 0 = sin tope
 : "${TIMEOUT:=60}"
 : "${LOG_LEVEL:=info}"
+: "${RUN_MIGRATIONS:=true}"
+: "${RUN_COLLECTSTATIC:=true}"
 
-python manage.py migrate --noinput
+# Detect available cores from CGroup in Kubernetes (if enabled)
+get_cpu_limit() {
+  if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ] && [ -f /sys/fs/cgroup/cpu/cpu.cfs_period_us ]; then
+    quota=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+    period=$(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)
+
+    if [ "$quota" -gt 0 ] && [ "$period" -gt 0 ]; then
+      echo $((quota / period))
+      return
+    fi
+  fi
+  nproc
+}
+
+# Calculate WORKERS for gunicorn
+CPU_CORES=$(get_cpu_limit)
+WORKERS=$(awk "BEGIN { w = int($CPU_CORES * $GUNICORN_WORKERS_PER_CORE); print (w < 1 ? 1 : w) }")
+if [ "$GUNICORN_MAX_WORKERS" -gt 0 ] && [ "$WORKERS" -gt "$GUNICORN_MAX_WORKERS" ]; then
+  WORKERS=$GUNICORN_MAX_WORKERS
+fi
+
+# Run database migration
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  python manage.py migrate --noinput
+fi
+
+if [ "$RUN_COLLECTSTATIC" = "true" ]; then
+  python manage.py collectstatic --noinput
+fi
 
 # Start Gunicorn
-python -m gunicorn "$APP_MODULE" \
-        --workers "$WORKERS" \
-        --threads "$THREADS" \
+exec gunicorn "$APP_MODULE" \
         --worker-class uvicorn.workers.UvicornWorker \
+        --workers $WORKERS \
         --bind "$BIND" \
         --timeout "$TIMEOUT" \
         --log-level "$LOG_LEVEL" \
         --access-logfile "-" \
-        --error-logfile "-"
+        --error-logfile "-" 
