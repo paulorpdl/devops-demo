fa4a75998f9e694befb5d344644af3a7e5d77b0b
Paulo Pinto <paulorpinto@gmail.com>
2025-06-17 15:56:58 -0500

    remove static file recollection.

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,10 +38,6 @@ if [ "$RUN_MIGRATIONS" = "true" ]; then
   python manage.py migrate --noinput
 fi
 
-if [ "$RUN_COLLECTSTATIC" = "true" ]; then
-  python manage.py collectstatic --noinput
-fi
-
 # Start Gunicorn
 exec gunicorn "$APP_MODULE" \
         --worker-class uvicorn.workers.UvicornWorker \
