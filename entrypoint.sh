b10db25139152accf3c615da608c162e0d9aceab
Paulo Pinto <paulorpinto@gmail.com>
2025-06-17 11:02:08 -0500

    improvements on entrypoint to increase configurability.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Detecting available cores
+CPU_CORES=$(nproc)
+
+: "${APP_MODULE:=demo.asgi}"
+: "${WORKERS:=$(( 2 * CPU_CORES + 1))}"
+: "${THREADS:=2}"
+: "${BIND:=0.0.0.0:8000}"
+: "${TIMEOUT:=60}"
+: "${LOG_LEVEL:=info}"
+
 python manage.py migrate --noinput
 
 # Start Gunicorn
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 demo.wsgi
+python -m gunicorn "$APP_MODULE" \
+        --workers "$WORKERS" \
+        --threads "$THREADS" \
+        --worker-class uvicorn.workers.UvicornWorker \
+        --bind "$BIND" \
+        --timeout "$TIMEOUT" \
+        --log-level "$LOG_LEVEL" \
+        --access-logfile "-" \
+        --error-logfile "-"
